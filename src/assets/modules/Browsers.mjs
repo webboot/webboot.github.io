a644c5bc24279b450f4c584c206092fdb6584678
jascha <jascha@jaeh.at>
2020-02-13 01:04:36 +0100

    add browser logos and Browsers

---
+++ src/assets/modules/Browsers.mjs
@@ -0,0 +1,33 @@
+export const View = () =>
+  List([
+    {
+      img: '/browsers/firefox.jpg',
+      to: '#',
+      title: 'firefox',
+      text: 'coming soon',
+    },
+    {
+      img: '/browsers/brave.jpg',
+      to: '#',
+      title: 'brave',
+      text: 'coming soon',
+    },
+    {
+      img: '/browsers/edge.jpg',
+      to: '#',
+      title: 'edge',
+      text: 'coming soon',
+    },
+    {
+      img: '/browsers/chromium.jpg',
+      to: '#',
+      title: 'chromium',
+      text: 'coming soon',
+    },
+    {
+      img: '/browsers/opera.jpg',
+      to: '#',
+      title: 'opera',
+      text: 'coming soon',
+    },
+  ])
