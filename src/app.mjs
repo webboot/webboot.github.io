294ee9031267ec8798d144bdf866af1eb014ea3f
jascha <jascha@jaeh.at>
2020-02-18 14:59:26 +0100

    add hero and footer data

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,9 @@
 export const state = config => ({
   title: '@webboot documentation',
   description: 'solving the tofu problem, one boot at a time.',
+
+  branding: ['@web', 'boot docs'],
+
   seo: {
     name: '@webboot',
     url: `https://${config.URL}`,
@@ -14,9 +17,44 @@ export const state = config => ({
       url: 'https://jaeh.at',
     },
   },
+
   menu: [
     { to: '/#user', text: 'user' },
     { to: '/#publisher', text: 'developer' },
     { to: '/#publisher', text: 'publisher' },
   ],
+
+  hero: {
+    title: ['@web', 'boot'],
+    description: 'trust on every step',
+    content: ['automagically verify the integrity of homepages,', 'before you load them.'],
+
+    menu: [
+      { to: '/#publisher', text: 'publisher' },
+      { to: '/#publisher', text: 'developer' },
+      { to: '/#user', text: 'user' },
+    ],
+  },
+  footer: {
+    one: {
+      title: '@webboot',
+      before: ['trust on every step.', 'created by the @webboot dao.'],
+      menu: [{ to: 'https://webboot.org/privacy/', text: 'privacy notice' }],
+    },
+    two: {
+      title: 'sitemap',
+      menu: [{ to: '/', text: 'index' }],
+    },
+
+    three: {
+      title: 'social links',
+      menu: [
+        { to: 'https://www.npmjs.com/org/webboot', text: 'npm' },
+        { to: 'https://twitter.com/webbootorg', text: 'twitter' },
+        { to: 'https://keybase.io/webboot', text: 'keybase' },
+        { to: 'https://github.com/webboot', text: 'github' },
+        { to: 'https://gitlab.com/webboot', text: 'gitlab' },
+      ],
+    },
+  },
 })
